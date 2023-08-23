f4ed1d7c4ae191cf93d24d3c5a27f76dce0a4eaa
Muhammad Bilal Khalid <64368766+bilalkhalidshaikh@users.noreply.github.com>
2023-08-23 20:47:55 +0500

    Added new Files

--- src/views/admin/requested/index.js
+++ src/views/admin/requested/index.js
@@ -24,15 +24,11 @@ import { Add, Delete, Edit } from "@mui/icons-material";
 import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
 import SearchBar from "../customers/SearchBar";
 import axios from "axios";
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-
-
-
-
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -63,7 +59,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
   };
 }
 
@@ -71,16 +67,16 @@ const RequestedServices = () => {
   const BASE_URL = "https://api.raft-service.com";
   const API_KEY = "340304930490d9f0df90df90df9d0f9d0f";
 
-
   // const [requestedServices, setRequestedServices] = useState({
   //   pending: [],
   //   purchased: [],
   // });
   const [requestedServices, setRequestedServices] = useState([]);
   const [requestedPendingServices, setRequestedPendingServices] = useState([]);
-  const [requestedFinishedServices, setRequestedFinishedServices] = useState([]);
+  const [requestedFinishedServices, setRequestedFinishedServices] = useState(
+    []
+  );
 
-  
   const fetchRequestedServices = async () => {
     const headers = {
       "Content-Type": "application/json",
@@ -88,13 +84,16 @@ const RequestedServices = () => {
     };
 
     try {
-      const response = await axios.get(`${BASE_URL}/rafts/get-service-requests`, {
-        headers,
-      });
+      const response = await axios.get(
+        `${BASE_URL}/rafts/get-service-requests`,
+        {
+          headers,
+        }
+      );
       const serviceData = response.data.data;
       const { pending, finished } = response.data.data;
 
-      setRequestedPendingServices(pending)
+      setRequestedPendingServices(pending);
       setRequestedFinishedServices(finished);
 
       setRequestedServices(serviceData);
@@ -110,7 +109,6 @@ const RequestedServices = () => {
   useEffect(() => {
     fetchRequestedServices();
   }, []);
-  
 
   const formatDate = (date) => {
     if (!date) return "";
@@ -133,7 +131,6 @@ const RequestedServices = () => {
     setValue(index);
   };
 
-
   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
@@ -143,86 +140,91 @@ const RequestedServices = () => {
         <br />
         <br />
         <br />
-        <Box sx={{ bgcolor: 'background.paper' }}>
-      <AppBar position="static" sx={{backgroundColor:"#11047A",color:'#eee'}}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="secondary"
-          textColor="inherit"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Pending" {...a11yProps(0)} />
-          <Tab label="Finished" {...a11yProps(1)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-      <TabPanel value={value} index={0} dir={theme.direction}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-              <TableCell>Vessel's Name</TableCell>
-              <TableCell>Equipment Name</TableCell>
-              <TableCell>Unit Price</TableCell>
-              <TableCell>Requested On</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            {requestedPendingServices.length ? (
-              requestedPendingServices.map((service) => (
-                <TableRow key={service?._id}>
-                  <TableCell>{service?.vessel?.vesselName}</TableCell>
-                  <TableCell>{service?.equipment?.eq_name}</TableCell>
-                  <TableCell>{service?.equipment?.unitPrice}</TableCell>
-                  <TableCell>{service?.ordered_on}</TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={4}>Nothing to show here</TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-          
-          </Table>
-        </TableContainer>
-      </TabPanel>
-      
-      <TabPanel value={value} index={1} dir={theme.direction}>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Vessel's Name</TableCell>
-                <TableCell>Customer Name</TableCell>
-                <TableCell>Raft Name</TableCell>
-                <TableCell>Requested On</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {requestedFinishedServices.map((service) => (
-                <TableRow key={service?._id}>
-                  <TableCell>{service?.vessel?.vesselName}</TableCell>
-                  <TableCell>{service?.customer?.customerName}</TableCell>
-                  <TableCell>{service?.raft?.raftName}</TableCell>
-                  <TableCell>{service?.ordered_on}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </TabPanel>
-      
-      </SwipeableViews>
-    </Box>
-      
+        <Box sx={{ bgcolor: "background.paper" }}>
+          <AppBar
+            position="static"
+            sx={{ backgroundColor: "#11047A", color: "#eee" }}
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab label="Pending" {...a11yProps(0)} />
+              <Tab label="Finished" {...a11yProps(1)} />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Vessel's Name</TableCell>
+                      <TableCell>Customer Name</TableCell>
+                      <TableCell>Raft Name</TableCell>
+                      <TableCell>Requested On</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {requestedPendingServices.length ? (
+                      requestedPendingServices.map((service) => (
+                        <TableRow key={service?._id}>
+                          <TableCell>{service?.vessel?.vesselName}</TableCell>
+                          <TableCell>
+                            {service?.customer?.customerName}
+                          </TableCell>
+                          <TableCell>{service?.raft?.raftName}</TableCell>
+                          <TableCell>
+                            {formatDate(new Date(service?.ordered_on))}
+                          </TableCell>
+                        </TableRow>
+                      ))
+                    ) : (
+                      <TableRow>
+                        <TableCell colSpan={4}>Nothing to show here</TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </TabPanel>
 
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Vessel's Name</TableCell>
+                      <TableCell>Customer Name</TableCell>
+                      <TableCell>Raft Name</TableCell>
+                      <TableCell>Requested On</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {requestedFinishedServices.map((service) => (
+                      <TableRow key={service?._id}>
+                        <TableCell>{service?.vessel?.vesselName}</TableCell>
+                        <TableCell>{service?.customer?.customerName}</TableCell>
+                        <TableCell>{service?.raft?.raftName}</TableCell>
+                        <TableCell>
+                          {formatDate(new Date(service?.ordered_on))}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </TabPanel>
+          </SwipeableViews>
+        </Box>
       </Container>
     </ThemeProvider>
   );
